04a2f387dfd2637b7213c4e960df3574348d5959
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-21 19:02:50 +0530

    Update rangeloop.cpp

--- c++/rangeloop.cpp
+++ c++/rangeloop.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -15,22 +15,4 @@ int main(){
         }
         cout<<n<<endl;
     }
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    int data[]={1,10,-1,5,6,-1,7,-99,8,10};
-    for(auto n :data)
-    {
-        if(n==-99)
-        {
-            break;
-        }
-        else if(n==-1)
-        {
-            continue;
-        }
-        cout<<n<<endl;
-    }
->>>>>>> 9a9eeff8ce8d2c9e5d9d980d198a6a1557567a89
 }
