118e49a88115af7c9bd199a7885b9a4c33c4befd
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-21 19:00:50 +0530

    Update forloop6.cpp

--- c++/forloop6.cpp
+++ c++/forloop6.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main()
@@ -31,38 +31,11 @@ int main()
         cout<<endl;
     }
     return 0;
-=======
-#include<iostream>
-using namespace std;
-int main()
-{
-    int i,j,space,rows;
-    cout<<"enter no of rows:";
-    cin>>rows;
-    for(i=1;i<=rows;i++)
-    {
-        for(space=1;space<=rows-i;space++)
-        {
-            cout<<" ";
-        }
-        for(j=1;j<=2*i-1;j++)
-        {
-        cout<<"*";
-        }
-        cout<<endl;
-    }
-    for(i=rows-1;i>0;i--)
-    {
-        for(int space=1;space<=(rows-i);space++)
-        {
-            cout<<" ";
+
         }
         for(j=1;j<=2*i-1;j++)
         {
             cout<<"*";
         }
         cout<<endl;
-    }
-    return 0;
->>>>>>> 9a9eeff8ce8d2c9e5d9d980d198a6a1557567a89
 }
