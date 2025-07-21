e99121b3e3cf2647f12d70716924f8c54eb5fac3
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-21 19:03:09 +0530

    Update sumarray.cpp

--- c++/sumarray.cpp
+++ c++/sumarray.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -13,20 +13,5 @@ int main(){
     }
     cout<<"Total is: "<<total;
     return 0;
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    int data[]={10,1,3,-4,7,5};
-    int total=0;
-    for(int i=0;i<6;i++)
-    {
-        for(int j=i+1;j<6;j++)
-        {
-            total +=data[i]*data[j];
-        }
-    }
-    cout<<"Total is: "<<total;
-    return 0;
->>>>>>> 9a9eeff8ce8d2c9e5d9d980d198a6a1557567a89
+
 }
