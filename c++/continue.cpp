144f880f117130449f6c6fe10679e069e383781f
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-21 18:55:37 +0530

    Update continue.cpp

--- c++/continue.cpp
+++ c++/continue.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -16,23 +16,3 @@ int main(){
 }
 cout<<total;
 return 0;
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    int n,total=0;
-
-    for(int i=1;i<=10;i++)
-    {
-        cout<<"enter a number:"<<endl;
-        cin>>n;
-        if(i<0)
-        {
-        continue ;
-    }
-    total +=n;
-}
-cout<<total;
-return 0;
->>>>>>> 9a9eeff8ce8d2c9e5d9d980d198a6a1557567a89
-}
