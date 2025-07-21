6d6b02fd5f086f08a759cbee93fdd8955b4e77b6
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-21 18:59:55 +0530

    Update forloop5.cpp

--- c++/forloop5.cpp
+++ c++/forloop5.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main()
@@ -17,24 +17,4 @@ int main()
         cout<<endl;
     }
     return 0;
-=======
-#include<iostream>
-using namespace std;
-int main()
-{
-    int i,j;
-    for(i=5;i>0;i--)
-    {
-        for(int space=1;space<=(5-i);space++)
-        {
-            cout<<" ";
-        }
-        for(j=1;j<=2*i-1;j++)
-        {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-    return 0;
->>>>>>> 9a9eeff8ce8d2c9e5d9d980d198a6a1557567a89
 }
