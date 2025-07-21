0cd0066c26d0e632a964859b6cc7890b1967473d
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-21 19:02:01 +0530

    Update nestedloop.cpp

--- c++/nestedloop.cpp
+++ c++/nestedloop.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -11,18 +11,4 @@ int main(){
         cout<<endl;
     }
     return 0;
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    for(int i=1;i<=4;i++)
-    {
-        for(int j=1;j<=5;j++)
-        {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-    return 0;
->>>>>>> 9a9eeff8ce8d2c9e5d9d980d198a6a1557567a89
 }
