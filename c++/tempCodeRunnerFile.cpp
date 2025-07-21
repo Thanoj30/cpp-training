db5775e095455850028fc2f2e2a2d85fe34852c8
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-21 19:03:25 +0530

    Update tempCodeRunnerFile.cpp

--- c++/tempCodeRunnerFile.cpp
+++ c++/tempCodeRunnerFile.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 int main()
@@ -17,24 +17,4 @@ int main()
         cout<<endl;
     }
     return 0;
-=======
-#include<iostream>
-using namespace std;
-int main()
-{
-    int i,j;
-    for(i=1;i<=5;i++)
-    {
-        for(int space=1;space<=(5-i);space++)
-        {
-            cout<<" ";
-        }
-        for(j=1;j<=i;j++)
-        {
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-    return 0;
->>>>>>> 9a9eeff8ce8d2c9e5d9d980d198a6a1557567a89
 }
