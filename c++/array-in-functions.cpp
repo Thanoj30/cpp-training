bb260fe0d8c682222aa14c768a63f59b68638910
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-27 10:00:26 +0530

    27-07

---
+++ c++/array-in-functions.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void print_array(const int[],int);
+void set_array(int[],int);
+int main(){
+    int arr[]={1,2,3,4,5};
+    int size=sizeof(arr)/sizeof(int);
+    print_array(arr,size);
+    set_array(arr,size);
+    print_array(arr,size);
+}
+void print_array(const int a[],int size){
+    for(int i=0;i<size;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+void set_array(int a[],int size){
+    for(int i=0;i<size;i++){
+        a[i]=a[i]*2;
+    }
+    cout<<"Array elements after modification: ";
+}
