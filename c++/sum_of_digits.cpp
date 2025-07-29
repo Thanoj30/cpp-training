0d52bb06db7eea404a0269e31a8c9bcae22414d9
K THANOJ REDDY <tthanoj18@gmail.com>
2025-07-29 07:50:12 +0530

    Create sum_of_digits.cpp
    29

---
+++ c++/sum_of_digits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int sum_of_digits(int n)
+{
+    //base case
+    if(n==0)
+    {
+        return 0;
+    }
+    return (n%10+sum_of_digits(n/10));
+    
+}
+int main(){
+    int n;
+    cout<<"Enter number to add its digits:";
+    cin>>n;
+    cout<<endl;
+    sum_of_digits(n);
+    cout<<"sum of digits:"<<sum_of_digits(n);
+}
