095b3212a53b9f8e45a7566f76978613ffe8879f
K THANOJ REDDY <tthanoj18@gmail.com>
2025-08-17 18:19:21 +0530

    Create modularMultipilication.cpp

---
+++ cpp_practice/modularMultipilication.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+const long long MOD = 1000000007;
+
+int main() {
+    long long ans = 1;
+    for (int i = 1; i <= 212; i++) {
+        ans = (ans * i) % MOD;  // modular multiplication
+    }
+    cout << "212! mod 1e9+7 = " << ans << endl;
+    return 0;
+}
