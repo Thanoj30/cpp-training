10a45b3f725786430b85097d90ca11e99072e3af
K THANOJ REDDY <tthanoj18@gmail.com>
2025-08-26 19:20:14 +0530

    Update pointers.cpp

--- cpp_practice/pointers/pointers.cpp
+++ cpp_practice/pointers/pointers.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
-//void function(int i,int ptr,int n){
-//      i+=1;
-//      ptr+1;
-//  }
-//  int main(){
-//      int i=4;
-//      int n=0;
-//      int *ptr=&i;
-//      function(i,*ptr,n);
-//      i=i+1;
-//      cout<<ptr<<endl;
-//      cout<<ptr<<endl;
-//      cout<<ptr<<endl;
-//}
-int main(){
-
-
-int first=110;
-int *p=&first;
-int **q=&p;
-int second =(**q)++ +9;
-cout<<first<<" "<<second<<endl;
-return 0;
+void function(int i,int ptr,int n){
+      i+=1;
+      ptr+1;
+  }
+  int main(){
+      int i=4;
+      int n=0;
+      int *ptr=&i;
+      function(i,*ptr,n);
+      i=i+1;
+      cout<<ptr<<endl;
+      cout<<ptr<<endl;
+     cout<<ptr<<endl;
 }
+// int main(){
+
+
+// int first=110;
+// int *p=&first;
+// int **q=&p;
+// int second =(**q)++ +9;
+// cout<<first<<" "<<second<<endl;
+// return 0;
+//}
