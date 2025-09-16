6acfc925b70fd02f6e863df93e88c9f8f93fe498
K THANOJ REDDY <tthanoj18@gmail.com>
2025-09-16 18:56:25 +0530

    Update 2dArray.cpp

--- cpp_practice/1d_2d_arrays/2dArray.cpp
+++ cpp_practice/1d_2d_arrays/2dArray.cpp
@@ -9,7 +9,7 @@ int main(){
     }
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
-            cin>>arr[i][j];
+            cin>>arr[i][j]; 
         } 
     }
     for(int i=0;i<row;i++){
