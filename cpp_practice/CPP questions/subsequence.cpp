731dec3ca10bf73252887a3081bf2cebb86791c1
K THANOJ REDDY <tthanoj18@gmail.com>
2025-10-26 11:18:10 +0530

    Update subsequence.cpp

--- cpp_practice/CPP questions/subsequence.cpp
+++ cpp_practice/CPP questions/subsequence.cpp
@@ -1,6 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
-
 void solve(vector<string>& ans, string str, string output, int i) {
     // base case
     if(i >= str.length()) {
@@ -8,7 +10,6 @@ void solve(vector<string>& ans, string str, string output, int i) {
             ans.push_back(output);
         return;
     }
-
     // Exclude the current character
     solve(ans, str, output, i+1);
 
@@ -16,24 +17,19 @@ void solve(vector<string>& ans, string str, string output, int i) {
     output.push_back(str[i]);
     solve(ans, str, output, i+1);
 }
-
 vector<string> subsequences(string str){
     vector<string> ans;
     string output = "";
     solve(ans, str, output, 0);
     return ans;
 }
-
 int main() {
     string str;
     cin >> str;
-
     vector<string> ans = subsequences(str);
-
     cout << "Subsequences are:\n";
     for(string s : ans) {
         cout << s << endl;
     }
-
     return 0;
 }
